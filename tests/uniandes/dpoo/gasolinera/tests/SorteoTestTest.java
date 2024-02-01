dc542244b25d3c3eb7a99d78b0945f31e8585f45
ASUS <ASUS@172.19.5.93>
2024-02-01 11:48:06 -0500

    Entrega final de taller 1 dpoo

---
+++ tests/uniandes/dpoo/gasolinera/tests/SorteoTestTest.java
@@ -0,0 +1,19 @@
+package uniandes.dpoo.gasolinera.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SorteoTestTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
